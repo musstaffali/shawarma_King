dcdae18bf3e3016c96c486a350fa4ddbacad0250
Mustafa Mustafa <mamustafa@gtcc.edu>
2020-10-24 20:03:52 -0400

    heroku 3

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+mysql://r81odcwjajn5nhub:xb2479978tfuxbrj@rnr56s6e2uk326pj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/x2yqt8ba9y9s2tii
+
 // Export the database functions for the controller (shawramaController.js).
 var routes = require("./controllers/shawarmas_controller.js");
 app.use(routes);
